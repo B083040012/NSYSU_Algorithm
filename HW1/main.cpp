94cb120adea517cbe8921f0b84beb1242a9f6969
b083040012@g-mail.nsysu.edu.tw <b083040012@g-mail.nsysu.edu.tw>
2021-11-02 23:38:23 +0800

    modified for timeout

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,6 +1,43 @@
 #include "ES.h"
+#include<unistd.h>
+#include<signal.h>
+#include<stdio.h>
+#include<sys/stat.h>
 
-int main(){
-    ES myES("ds2");
-    myES.process();
+bool timeSet=false;
+
+ES myES;
+
+void handler(int sigNum){
+    if(sigNum==SIGALRM){
+        cout << "over time (60 seconds)\n";
+        myES.modSet();
+    }
+}
+
+int main(int argc, char *argv[]){
+    const int dir_err = mkdir("answer", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    string inputFile;
+    vector<string> command;
+    if(argc>1){
+        command.assign(argv,argv+argc);
+        for(int i=1;i<argc;i++){
+            if(command[i]=="-input"){
+                inputFile=command[i+1];
+                i++;
+            }
+        }
+    }
+
+    myES.setBackpack(inputFile);
+    // set timer if timeout
+    signal(SIGALRM,handler);
+    alarm(60);
+    myES.esMethod();
+    cout << "\n\n";
+    myES.show();
+    if(myES.gettimeSet()) cout << "not done in 1 minute\n";
+    cout << "writing ans file...\n";
+    myES.writeTxt();
+    cout << "complete writing\n";
 }
